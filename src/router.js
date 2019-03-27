1856f66e5d9e582f0f4583b7a82c0b5386109af1
Lino004 <h.ayinlamouhayad@gmail.com>
2019-03-27 16:26:02 +0100

    Initialisation du projet

--- src/router.js
+++ src/router.js
@@ -1,23 +1,37 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Home from '@/components/home/Index.vue'
+import Vitrine from './views/Vitrine.vue'
+import App from './views/App.vue'
 
 Vue.use(Router)
 
+const DEFAULT_PAGE = '/vitrine'
+
 export default new Router({
+  mode: 'history',
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: Home
+      path: '*',
+      redirect: DEFAULT_PAGE
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '/vitrine',
+      name: 'vitrine',
+      component: Vitrine
+    },
+    {
+      path: '/',
+      name: 'App',
+      component: App,
+      redirect: DEFAULT_PAGE,
+      children: [
+        {
+          path: '/home',
+          name: 'home',
+          component: Home
+        }
+      ]
     }
   ]
 })
---
+++ src/views/Vitrine.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <section class="hero is-fullheight">
+      <div class="hero-body">
+        <div class="container has-text-centered">
+          <div class="column is-4 is-offset-4">
+            <h3 class="title has-text-grey">Page Vitrine</h3>
+            <div>
+              <router-link
+                :to="{name: 'home'}"
+                class="subtitle has-text-grey">
+                Cliquez ici pour resoindre l'accueil
+              </router-link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'vitrine',
+  components: {}
+}
+</script>
