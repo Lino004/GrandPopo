8d2824ea9d46d834f3c449760e0aa077a0e84b56
Lino004 <h.ayinlamouhayad@gmail.com>
2019-04-11 14:07:22 +0100

    suppression de buefy et ajout de semantic ui

--- src/components/home/Index.vue
+++ src/components/home/Index.vue
@@ -1,11 +1,28 @@
 <template>
-  <div class="has-text-centered has-background-white">
-    <h1 class="title has-text-dark">Home</h1>
-    <div>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-      Corrupti non maiores iste reiciendis, excepturi dolor soluta,
-      expedita vero eveniet ullam obcaecati aliquid eius itaque laudantium
-      distinctio quia est sequi nesciunt!
+  <div class="ui container">
+    <div class="ui very relaxed items">
+      <div class="item">
+        <div class="image">
+          <img src="https://semantic-ui.com/images/wireframe/image.png">
+        </div>
+        <div class="content">
+          <a class="ui header" href="#link">Link Header</a>
+          <div class="description">
+            <p>Ut imperdiet dignissim feugiat. Phasellus tristique odio eu justo dapibus, nec rutrum ipsum luctus. Ut posuere nec tortor eu ullamcorper. <a href="#link">Etiam pellentesque</a> tincidunt tortor, non sagittis nibh pretium sit amet. Sed neque dolor, blandit eu ornare vel, lacinia porttitor nisi. Vestibulum sit amet diam rhoncus, consectetur enim sit amet, interdum mauris. Praesent feugiat finibus quam, porttitor varius est egestas id.</p>
+          </div>
+        </div>
+      </div>
+      <div class="item">
+        <div class="image">
+          <img src="https://semantic-ui.com/images/wireframe/image.png">
+        </div>
+        <div class="content">
+          <a class="ui header" href="#link">Link Header</a>
+          <div class="description">
+            <p>Ut imperdiet dignissim feugiat. Phasellus tristique odio eu justo dapibus, nec rutrum ipsum luctus. Ut posuere nec tortor eu ullamcorper. <a href="#link">Etiam pellentesque</a> tincidunt tortor, non sagittis nibh pretium sit amet. Sed neque dolor, blandit eu ornare vel, lacinia porttitor nisi. Vestibulum sit amet diam rhoncus, consectetur enim sit amet, interdum mauris. Praesent feugiat finibus quam, porttitor varius est egestas id.</p>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
--- src/views/Vitrine.vue
+++ src/views/Vitrine.vue
@@ -1,22 +1,14 @@
 <template>
   <div>
-    <section class="hero is-fullheight">
-      <div class="hero-body">
-        <div class="container has-text-centered">
-          <div class="column is-4 is-offset-4">
-            <h3 class="title has-text-info">Page Vitrine</h3>
-            <div>
-              <a>
-                <router-link
-                  :to="{name: 'home'}"
-                  class="subtitle has-text-black">
-                  Cliquez ici pour resoindre l'accueil
-                </router-link>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
+    <section>
+      <h3 class="title has-text-info">Page Vitrine</h3>
+      <a>
+        <router-link
+          :to="{name: 'home'}"
+          class="subtitle has-text-black">
+          Cliquez ici pour resoindre l'accueil
+        </router-link>
+      </a>
     </section>
   </div>
 </template>
